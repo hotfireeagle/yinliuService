ca4ecceca231a5b1a09c5e7be0bdf6b978561842
hotfireeagle <hotfireeagle@163.com>
2021-05-09 23:34:44 +0800

    引入go fiber

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module yinliuService
 
 go 1.14
+
+require github.com/gofiber/fiber/v2 v2.9.0 // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gofiber/fiber/v2"
+)
+
+func main() {
+	app := fiber.New()
+
+	app.Get("/", func(c *fiber.Ctx) error {
+		return c.SendString("Hello, World!")
+	})
+
+	err := app.Listen(":3000")
+	if err != nil {
+		fmt.Println("服务器启动失败")
+	}
+}
